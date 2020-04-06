383a2d38f6eb27699a30747f6f72ac5cd9c678e9
GreatlightSaber <49675601+GreatlightSaber@users.noreply.github.com>
2020-04-06 16:09:48 +0900

    - 정렬 문제 해결 완료

--- algorithm/src/sort/soluton2.java
+++ algorithm/src/sort/soluton2.java
@@ -1,5 +1,8 @@
 package sort;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -20,97 +23,47 @@ import org.junit.jupiter.api.Test;
 public class soluton2 {
 	@Test
 	public void test() {
-		int[] numbers = {6,10,2};
-		//int[] numbers = {3, 30, 34, 5, 9};
-		// solution(numbers);
-		String temp = "12345";
-		System.out.println(temp.substring(1,2));
+		//int[] numbers = {6,10,2};
+		int[] numbers = {3, 30, 34, 5, 9};
+		solution(numbers);
 	}
 	public String solution(int[] numbers) {
         String answer = "";
-        // 문자열로 변경
-        String[] arrStrNumbers = new String[numbers.length];
-        for(int i = 0; i < numbers.length ; i++) {
-        	arrStrNumbers[i] = Integer.toString(numbers[i]);
-        }
-        
-        // 정렬(선택정렬)
-        for(int num1 = 0 ; num1 < arrStrNumbers.length;num1++) {
-        	int iMax = num1;
-        	for(int num2 = num1 ; num2 < numbers.length ; num2++) {
-        		// 앞자리 비교
-        		String strNumber = arrStrNumbers[num2];
-        		String strMaxNumber = arrStrNumbers[iMax];
-        		int strLength = strNumber.length() < strMaxNumber.length() ? strNumber.length() : strMaxNumber.length();
-        		for(int num3 = 0 ; num3 < strLength ; num3++) {
-        			if(strNumber.substring(beginIndex))
-        		}
-        		
-        	}
-        	if(iMax != num1) {
-				int temp = numbers[num1];
-				numbers[num1] = numbers[iMax];
-				numbers[iMax] = temp;
-			}
-        }
-        // 자릿수마다 일일히 비교
-        
-        
-        
-        
-        
-        
-        
-        
-        // 모든 numbers의 첫째 자리 수만 비교하여 내림차순 정렬
         
         // 정렬(버블정렬)
-//        for(int num1 = 0; num1 < numbers.length-1 ; num1++) {
-//        	for(int num2 = 0; num2 < numbers.length-1 ; num2++) {
-//        		double number1 = numbers[num2];
-//        		double number2 = numbers[num2+1];
-//	          	number1 = number1 / 10 < 1 ? number1 : number1 / 10;
-//	          	number2 =number2 / 10 < 1 ? number2 : number2 / 10;
-//	          	if(number1 == number2) {
-//	          		if(numbers[num2] >= 10 && numbers[num2+1] < 10) {
-//	          			int temp = numbers[num2];
-//		          		numbers[num2] = numbers[num2+1];
-//		          		numbers[num2+1] = temp;
-//	          		}
-//	          	} else if(number1 < number2) {
-//	          		int temp = numbers[num2];
-//	          		numbers[num2] = numbers[num2+1];
-//	          		numbers[num2+1] = temp;
-//	          	}
+//        for(int num1 = numbers.length-1; num1 > 0 ; num1--) {
+//        	for(int num2 = 0; num2 < num1 ; num2++) {
+//        		String strNumber1 = Integer.toString(numbers[num2]);
+//        		String strNumber2 = Integer.toString(numbers[num2+1]);
+//        		if((strNumber1 + strNumber2).compareTo(strNumber2 + strNumber1) < 0) {
+//        			int temp = numbers[num2];
+//        			numbers[num2] = numbers[num2+1];
+//        			numbers[num2+1] = temp;
+//        		}
 //        	}
 //        }
-        // 정렬(선택 정렬)
-//        for(int num1 = 0 ; num1 < numbers.length;num1++) {
-//        	int iMax = num1;
-//        	for(int num2 = num1 ; num2 < numbers.length ; num2++) {
-//        		double number1 = numbers[num2];
-//        		double number2 = numbers[iMax];
-//	          	number1 = number1 / 10 < 1 ? number1 : number1 / 10;
-//	          	number2 =number2 / 10 < 1 ? number2 : number2 / 10;
-//	          	if(number1 == number2) {
-//	          		if(numbers[num2] < 10 && numbers[iMax] >= 10) {
-//	          			iMax = num2;
-//	          		}
-//	          	} else if(number1 > number2) {
-//	          		iMax = num2;
-//	          	}
-//        	}
-//        	if(iMax != num1) {
-//				int temp = numbers[num1];
-//				numbers[num1] = numbers[iMax];
-//				numbers[iMax] = temp;
-//			}
-//        }
+        Integer[] arrays = new Integer[numbers.length];
+        for(int i = 0; i< numbers.length ; i++) {
+        	arrays[i] = numbers[i];
+        }
+        
+        Arrays.sort(arrays, new Comparator<Integer>() {
+        	@Override
+        	public int compare(Integer o1, Integer o2) {
+        		 String tmp1 = o1.toString();
+                 String tmp2 = o2.toString();
+                 return ((tmp2+tmp1)).compareTo(tmp1+tmp2);
+        	}
+		});
         
         // 문자열 변경
-//        for(int number : numbers) {
-//        	answer += Integer.toString(number);
-//        }
+        for(Integer number : arrays) {
+        	answer += Integer.toString(number);
+        }
+        if("0".equals(answer.substring(0, 1)))
+            return "0";
+        
+        
         return answer;
     }
 }
---
+++ algorithm/src/sort/soluton3.java
@@ -0,0 +1,54 @@
+package sort;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * k번째지수
+ * 
+ * - 문제설명
+ * H-Index는 과학자의 생산성과 영향력을 나타내는 지표입니다. 어느 과학자의 H-Index를 나타내는 값인 h를
+ * 구하려고 합니다. 위키백과1에 따르면, H-Index는 다음과 같이 구합니다.
+ * 
+ * 어떤 과학자가 발표한 논문 n편 중, h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용
+ * 되었다면 h의 최댓값이 이 과학자의 H-Index입니다. 
+ *  
+ *  어떤 과학자가 발표한 논문의 인용 횟수를 담은 배열 citations가 매개변수로 주어질 때, 이 과학자의 H-Index
+ *  를 return 하도록 solution 함수를 작성해주세요.
+ *  
+ * - 제한사항
+ * numbers의 길이는 1 이상 100,000 이하입니다.
+ * numbers의 원소는 0 이상 1,000 이하입니다.
+ * commands의 길이는 1 이상 50 이하입니다. 
+ * 정답이 너무 클 수 있으니 문자열로 바꾸어 return 합니다.
+ * 
+ * */
+public class soluton3 {
+	@Test
+	public void test() {
+		// int[] citations = {3, 0, 6, 1, 5};
+		int[] citations = {0, 0, 0, 0, 0};
+		int result = solution(citations);
+		System.out.println(Integer.toString(result));
+	}
+	 public int solution(int[] citations) {
+		 int[] count = new int[citations.length];
+
+	        for(int i=0; i<citations.length ; i++) {
+	            for(int j=0 ; j<citations.length ; j++) {
+	                if(citations[i] <= citations[j]) {
+	                    count[i]++;
+	                }
+	            }
+	        }
+
+	        int max = 0;
+	        for(int i=0 ; i<citations.length ; i++) {
+	            if(count[i] <= citations[i]) {
+	            	if(max <= count[i]) {
+	            		max = count[i];
+	            	}
+	            }
+	        }
+	        return max;
+	    }
+}
