43083ca0285c23e26c09a2f4c70dc704b661f5fd
GreatlightSaber <49675601+GreatlightSaber@users.noreply.github.com>
2020-04-07 11:32:06 +0900

    - 해시 > 완주하시 못한 선수 완료

---
+++ algorithm/src/hash/soluton2.java
@@ -0,0 +1,51 @@
+package hash;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * k번째지수
+ * 
+ * - 문제설명
+ * 수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주 하였습니다.
+ * 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion
+ * 이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+ *  
+ * - 제한사항
+ * 마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
+ * completion의 길이는 participant의 길이보다 1 작습니다.
+ * 참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
+ * 참가자 중에는 동명이인이 있을 수 있습니다. 
+ * 
+ * */
+public class soluton2 {
+	@Test
+	public void test() {
+//		String[] participant = {"leo", "kiki", "eden"};
+//		String[] completion = {"eden", "kiki"};
+		String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
+		String[] completion = {"josipa", "filipa", "marina", "nikola"};
+//		String[] participant = {"mislav", "stanko", "mislav", "ana"};
+//		String[] completion = {"stanko", "ana", "mislav"};
+		System.out.println(solution(participant, completion));
+	}
+	 public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        for(int i = 0 ; i < completion.length ; i ++) {
+        	String strParticipant = participant[i];
+        	String strCompletion = completion[i];
+        	if(!strParticipant.equals(strCompletion)) {
+        		answer = participant[i];
+        		break;
+        	} else if(i == completion.length -1) {
+        		answer = participant[i+1];
+        	}
+        }
+        
+        return answer;
+    }
+}
