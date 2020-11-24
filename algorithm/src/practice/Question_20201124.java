d952e2edd7abe7f45186a58aca7bd6f3f662d5d3
JCJ <JCJ@112.171.20.74>
2020-11-25 00:15:17 +0900

    - 무작위 알파벳을 입력 받으면 같은 쌍끼리 대칭으로 배열 생성

--- algorithm/src/practice/Question_20201124.java
+++ algorithm/src/practice/Question_20201124.java
@@ -7,10 +7,13 @@ public class Question_20201124 {
 	// 	1) 쌍이 없는값은 버린다
 	
 	public static void main (String[] args) {
-		
+		String[] input = {"G","M","Q","D","M","A","J","D","O","J","A","U","E","J","E","B",};
+		solution(input);
 	}
 	
 	public static void solution(String[] input) {
+		
+		ArrayList<String> result = new ArrayList<String>();
 		
 		// ArrayList 생성
 		ArrayList<String> arrayList = new ArrayList<String>();
@@ -18,15 +21,23 @@ public class Question_20201124 {
 			arrayList.add(input[i]);
 		}
 		
-		for(int i1 = 0; i1< input.length ; i1++) {
-			String temp1 = arrayList.get(i1);
-			if(arrayList.lastIndexOf(temp1) != i1) {
+		while(arrayList.size() > 0) {
+			String temp1 = arrayList.get(0);
+			int index = arrayList.lastIndexOf(temp1);
+			if(index != 0) {
+				// 값 저장(대칭으로 담을 수 있는 로직 구현 필요)
+				result.add(0,arrayList.get(0));
+				result.add(result.size(), arrayList.get(index));
 				
+				// 값을 저장 했으면 해당 값 삭제
+				arrayList.remove(index);
+				arrayList.remove(0);
 			}else {
-				arrayList.remove(i1);
+				// 쌍이 없는 값은 remove
+				arrayList.remove(0);
 			}
 		}
-		
+		System.out.println(result.toString());
 	}
 	
 }
